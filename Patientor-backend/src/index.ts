a9a760771a6cdf094f89f9fd1fe6a4722be99a5a
jennijavanainen <jenni.kupukupu@gmail.com>
2023-06-26 14:31:36 +0300

    9.10. fetching diagnoses

--- Patientor-backend/src/index.ts
+++ Patientor-backend/src/index.ts
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 const cors = require('cors')
 app.use(cors())
-
+import diagnoseRouter from "./routes/diagnoses";
 
 const PORT = 3001;
 
@@ -11,6 +11,8 @@ app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
